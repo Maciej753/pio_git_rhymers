62a36d929b4b112b809793021e2c643150630572
Igor-Plowens <recurshet@interia.pl>
2026-03-30 12:58:34 +0200

    Wprowadzanie zmian dział III.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,28 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Implementacja wyliczanki, która odrzuca niektóre elementy i zlicza ich liczbę.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Liczba odrzuconych elementów
+     */
     private int totalRejected = 0;
 
+    /**
+     * Zwraca liczbę odrzuconych elementów.
+     * @return liczba odrzuceń
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Dodaje element, jeśli spełnia warunek.
+     * W przeciwnym razie zwiększa licznik odrzuceń.
+     * @param in element do dodania
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
